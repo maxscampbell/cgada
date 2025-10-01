6023d2804983d4006d764c74975fa82531123ff3
mscampb4 <mscampb4@ncsu.edu>
2025-10-01 18:29:12 -0400

    Prepare 0.13.1 script using randomly-generated testing data.

--- compile_html.R
+++ compile_html.R
@@ -11,3 +11,8 @@ rmarkdown::render(input = paste0(source, "/", "CGA-0.13.rmd"),
                   output_format = "html_document",
                   output_file = "CGA-0.13.html",
                   output_dir = out)
+
+rmarkdown::render(input = paste0(source, "/", "CGA-0.13.1.rmd"),
+                  output_format = "html_document",
+                  output_file = "CGA-0.13.1.html",
+                  output_dir = out)
---
+++ test_data.R
@@ -0,0 +1,29 @@
+#Create a function that generates dummy data to use for testing purposes
+generate_test_data <- function(fileName = "test_data", n_obs = 10000) {
+  
+  base_class <- c("Alchemist", "Artillery", "Battlesmith", "Champion", "Chronomancer", 
+                  "Cloudbreaker", "Dancer", "Evincer", "Frost Knight", "Griefer", 
+                  "Hexblade", "Ranger", "Skirmisher", "Spectre", "Stargazer", 
+                  "Titan", "Vampire")
+  base_trinket <- c("Accelerator", "Bandage", "Echo Bolt", "Flame Trap", "Hedge Seed", 
+                    "Lava Cake", "Nox Bomb", "Risk-it-Biscuit", "Smoke Screen", "Spring Plume", 
+                    "Vigor Flask")
+  base_map <- c("Fissure", "Crystal", "Sunken", "Dustworks", "Polaris", 
+                "Scald", "Golden", "Bogpit", "Deepgrove", "Skullway", 
+                "Graphite")
+  
+  
+  Class <- sample(base_class, size = n_obs, replace = TRUE)
+  Trinket <- sample(base_trinket, size = n_obs, replace = TRUE)
+  Map <- sample(base_map, size = n_obs, replace = TRUE)
+  Kills <- sample(c(0:5), size = n_obs, replace = TRUE)
+  Coins <- sample(c(1:40), size = n_obs, replace = TRUE)
+  
+  out <- data.frame(Class, Trinket, Map, Kills, Coins)
+  write.csv(out, paste0("./data/", fileName, ".csv"), row.names = FALSE)
+  
+}
+
+generate_test_data("test_data", 500)
+generate_test_data("test_data2", 500)
+
