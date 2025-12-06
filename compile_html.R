935ff9b43ed9a81aff954e726b0ae7820f6ac22e
mscampb4 <mscampb4@ncsu.edu>
2025-12-06 12:23:17 -0500

    Update links in all reports and add "return to top" links in current report.

--- compile_html.R
+++ compile_html.R
@@ -8,12 +8,12 @@ rmarkdown::render(input = paste0(source, "/", "CGA-0.13.1.rmd"),
                   output_dir = out)
 
 #Legacy reports
-rmarkdown::render(input = paste0(source, "/", "CGA-Playtest-Analysis.rmd"),
+rmarkdown::render(input = paste0(source, "/", "CGA-Playtest-Analysis.Rmd"),
                   output_format = "html_document",
                   output_file = "CGA Playtest Analysis.html",
                   output_dir = out)
 
-rmarkdown::render(input = paste0(source, "/", "CGA-0.13.rmd"),
+rmarkdown::render(input = paste0(source, "/", "CGA-0.13.Rmd"),
                   output_format = "html_document",
                   output_file = "CGA-0.13.html",
                   output_dir = out)
